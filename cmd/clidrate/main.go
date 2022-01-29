e397a46e0736a93670608cbabe8a77b7bee609b1
Keydose <michaelkelly@myself.com>
2022-01-29 23:32:46 +0000

    Application working; added TODOs for improvements

--- cmd/clidrate/main.go
+++ cmd/clidrate/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/gen2brain/beeep"
+)
+
+type hydrateTimer struct {
+	startedAt    time.Time
+	interval     int
+	amount       int
+	hydrationDue bool
+}
+
+func (t hydrateTimer) getElapsedTimeInSeconds() int {
+	return int(time.Since(t.startedAt).Seconds())
+}
+
+func (t hydrateTimer) notify() {
+	message := fmt.Sprintf("Drink %dml of water", t.amount)
+	// TODO: Make the image smaller
+	beeep.Notify("Hydrate!", message, "../../assets/images/water-droplet.png")
+}
 
 func main() {
-	fmt.Println("Testing!")
+	hydrateTimer := hydrateTimer{
+		startedAt:    time.Now(),
+		interval:     10,
+		amount:       250,
+		hydrationDue: false,
+	}
+
+	// TODO: Unit test???
+
+	// TODO: Once passed interval, keep notifying every 30 seconds until
+	//   - The user confirms that they've hydrated in the CLI
+	prevElapsed := 0
+	for {
+		elapsed := hydrateTimer.getElapsedTimeInSeconds()
+		if elapsed != prevElapsed {
+			prevElapsed = elapsed
+			if elapsed%hydrateTimer.interval == 0 {
+				hydrateTimer.hydrationDue = true // TODO: Link in with CLI confirmation
+				hydrateTimer.notify()
+			}
+		}
+	}
 }
