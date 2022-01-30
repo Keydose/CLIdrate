3c3e9b2e727ddc668b20c8850d16ca72e0104072
Keydose <michaelkelly@myself.com>
2022-01-30 15:44:57 +0000

    Refactored to use command-line flags and a go ticker channel instead of a custom timer loop

--- cmd/clidrate/main.go
+++ cmd/clidrate/main.go
@@ -1,50 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gen2brain/beeep"
 )
 
-type hydrateTimer struct {
-	startedAt    time.Time
-	interval     int
-	amount       int
-	hydrationDue bool
-}
-
-func (t hydrateTimer) getElapsedTimeInSeconds() int {
-	return int(time.Since(t.startedAt).Seconds())
-}
-
-func (t hydrateTimer) notify() {
-	message := fmt.Sprintf("Drink %dml of water", t.amount)
+func notify(hydrateAmount int) {
+	message := fmt.Sprintf("Drink %dml of water", hydrateAmount)
+	fmt.Println("Hydrate notification sent")
 	// TODO: Make the image smaller
 	beeep.Notify("Hydrate!", message, "../../assets/images/water-droplet.png")
 }
 
-func main() {
-	hydrateTimer := hydrateTimer{
-		startedAt:    time.Now(),
-		interval:     10,
-		amount:       250,
-		hydrationDue: false,
-	}
+func tick(hydrateInterval int, hydrateAmount int) {
+	ticker := time.NewTicker(time.Duration(hydrateInterval) * time.Second)
+	done := make(chan bool)
 
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			go notify(hydrateAmount)
+		}
+	}
+}
+
+func main() {
+	hydrateIntervalPtr := flag.Int("interval", 3600, "Hydrate interval in seconds")
+	hydrateAmountPtr := flag.Int("amount", 250, "Hydrate amount in millilitres")
+
+	flag.Parse()
+
+	fmt.Fprintf(os.Stdout, "I'll remind you to hydrate by %dml every %d seconds", *hydrateAmountPtr, *hydrateIntervalPtr)
+
+	go tick(*hydrateIntervalPtr, *hydrateAmountPtr)
+	select {}
 	// TODO: Unit test???
 
 	// TODO: Once passed interval, keep notifying every 30 seconds until
 	//   - The user confirms that they've hydrated in the CLI
-	prevElapsed := 0
-	for {
-		elapsed := hydrateTimer.getElapsedTimeInSeconds()
-		if elapsed != prevElapsed {
-			prevElapsed = elapsed
-			if elapsed%hydrateTimer.interval == 0 {
-				hydrateTimer.hydrationDue = true // TODO: Link in with CLI confirmation
-				hydrateTimer.notify()
-			}
-		}
-	}
 }
